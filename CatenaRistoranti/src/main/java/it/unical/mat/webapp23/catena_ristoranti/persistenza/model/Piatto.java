83f0705a96099291a642b13b3e0c4d16c5e8eb5b
Kristian <kristian.reale@gmail.com>
2022-10-24 17:54:06 +0200

    Modello: piatti e ristoranti

---
+++ CatenaRistoranti/src/main/java/it/unical/mat/webapp23/catena_ristoranti/persistenza/model/Piatto.java
@@ -0,0 +1,32 @@
+package it.unical.mat.webapp23.catena_ristoranti.persistenza.model;
+
+import java.util.List;
+
+public class Piatto {
+	private Long id;
+	private String nome;
+	private List<Ristorante> ristoranti;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public List<Ristorante> getRistoranti() {
+		return ristoranti;
+	}
+	public void setRistoranti(List<Ristorante> ristoranti) {
+		this.ristoranti = ristoranti;
+	}
+	
+	
+	
+	
+}
