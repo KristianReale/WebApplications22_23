32363c0bef7bb609b935b4fc9c6cbf31347cc490
Kristian <kristian.reale@gmail.com>
2022-10-24 16:51:14 +0200

    first push

---
+++ CatenaRistoranti/src/main/java/it/unical/mat/webapp23/catena_ristoranti/CatenaRistorantiApplication.java
@@ -0,0 +1,13 @@
+package it.unical.mat.webapp23.catena_ristoranti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatenaRistorantiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatenaRistorantiApplication.class, args);
+	}
+
+}
---
+++ CatenaRistoranti/src/test/java/it/unical/mat/webapp23/catena_ristoranti/CatenaRistorantiApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unical.mat.webapp23.catena_ristoranti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatenaRistorantiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
