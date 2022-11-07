fab2a3b4b99b4684cc52fb1ef6771a860f91e6d4
Kristian <kristian.reale@gmail.com>
2022-11-07 18:36:58 +0100

    MVC Piatti serviti da ristoranti

--- CatenaRistoranti/src/main/java/it/unical/mat/webapp23/catena_ristoranti/controller/ListaRecensioni.java
+++ CatenaRistoranti/src/main/java/it/unical/mat/webapp23/catena_ristoranti/controller/ListaRecensioni.java
@@ -1,6 +1,7 @@
 package it.unical.mat.webapp23.catena_ristoranti.controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.RequestDispatcher;
@@ -11,16 +12,50 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import it.unical.mat.webapp23.catena_ristoranti.persistenza.DBManager;
+import it.unical.mat.webapp23.catena_ristoranti.persistenza.dao.PiattoDao;
+import it.unical.mat.webapp23.catena_ristoranti.persistenza.model.Piatto;
 import it.unical.mat.webapp23.catena_ristoranti.persistenza.model.Ristorante;
 
 
 @WebServlet("/recensioni")
 public class ListaRecensioni extends HttpServlet{
+	class RistorantePiatto {
+		Ristorante ristorante;
+		List<Piatto> piatti;
+		
+		public Ristorante getRistorante() {
+			return ristorante;
+		}
+		public void setRistorante(Ristorante ristorante) {
+			this.ristorante = ristorante;
+		}
+		public List<Piatto> getPiatti() {
+			return piatti;
+		}
+		public void setPiatti(List<Piatto> piatti) {
+			this.piatti = piatti;
+		}
+	}
+	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		List<RistorantePiatto> rPiatti = new ArrayList<RistorantePiatto>();
+		
 		List<Ristorante> ristoranti = DBManager.getInstance()
 							.getRistoranteDao().findAll();
-		req.setAttribute("lista_ristoranti", ristoranti);
+		
+		PiattoDao pDao = DBManager.getInstance().getPiattoDao();
+		for (Ristorante rist : ristoranti) {
+			RistorantePiatto rp = new RistorantePiatto();
+			List<Piatto> piatti = pDao.findByRestaurantLazy(rist);
+			rp.setRistorante(rist);
+			rp.setPiatti(piatti);
+			
+			rPiatti.add(rp);
+		}
+		
+		req.setAttribute("lista_ristoranti", rPiatti);
+		
 		
 		
 		RequestDispatcher dispacher = req.getRequestDispatcher("views/recensioni/recensioni.html");
