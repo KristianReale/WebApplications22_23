9d92bfd3793797a699c6c1c5ebbf39bb6b0c6176
Kristian <kristian.reale@gmail.com>
2022-11-14 18:32:12 +0100

    Piatti con prezzo e checkbox

--- CatenaRistoranti/src/main/resources/static/js/validate.js
+++ CatenaRistoranti/src/main/resources/static/js/validate.js
@@ -1,3 +1,12 @@
+function Piatto(id, prezzo){
+	this.id = id;
+	this.prezzo = prezzo;
+}
+
+
+
+piatti = new Object();
+
 function validateLogin(username, password, e){
 	if (username.value == ""){
 		alert("Inserisci username")
@@ -6,8 +15,14 @@ function validateLogin(username, password, e){
 	
 }
 
+function selezionaPiatto(idPiatto, prezzo){
+	var piatto = new Piatto(idPiatto, prezzo);
+	piatti[idPiatto] = piatto;
+}
+
 function validateRistorante(txtNome, txtDescrizione, txtUbicazione, ev){
 	var canSubmit = true;
+	console.log(txtNome.value);
 	if (txtNome.value.trim() == ""){
 		alert("Inserisci username")
 		canSubmit = false;
@@ -15,7 +30,13 @@ function validateRistorante(txtNome, txtDescrizione, txtUbicazione, ev){
 	if (txtDescrizione.value.trim() == ""){
 		alert("Inserisci descrizione")
 		canSubmit = false;
+	}else{
+		if (txtDescrizione.value.trim().length < 10){
+			alert("La descrizione deve essere di almeno 10 caratteri")
+			canSubmit = false;
+		}
 	}
+	
 	if (txtUbicazione.value.trim() == ""){
 		alert("Inserisci cap")
 		canSubmit = false;
