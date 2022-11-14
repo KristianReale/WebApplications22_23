87b504cd11181c0dd9e9f8b25b11a43f7d9eb616
Kristian <kristian.reale@gmail.com>
2022-11-14 17:26:26 +0100

    prevent default ristorante

--- CatenaRistoranti/src/main/resources/static/js/validate.js
+++ CatenaRistoranti/src/main/resources/static/js/validate.js
@@ -5,3 +5,23 @@ function validateLogin(username, password, e){
 	}
 	
 }
+
+function validateRistorante(txtNome, txtDescrizione, txtUbicazione, ev){
+	var canSubmit = true;
+	if (txtNome.value.trim() == ""){
+		alert("Inserisci username")
+		canSubmit = false;
+	}
+	if (txtDescrizione.value.trim() == ""){
+		alert("Inserisci descrizione")
+		canSubmit = false;
+	}
+	if (txtUbicazione.value.trim() == ""){
+		alert("Inserisci cap")
+		canSubmit = false;
+	}
+	if (!canSubmit){
+		ev.preventDefault();
+	}
+	
+}
